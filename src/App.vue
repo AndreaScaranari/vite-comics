9b02143765dffed4e0c7dc468c57631fd5a5329f
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-02 18:47:00 +0100

    AppHeader_hardcoded

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script setup>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
 
-<style scoped></style>
+<style>
+body {
+  background-color: black;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: "AppHeader",
+    data: () => ({
+        imageName: "dc-logo.png"
+    }),
+    methods: {
+        createImaginePath(imageName) {
+            const url = new URL(`../assets/img/${imageName}`, import.meta.url);
+            return url.href
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <img :src="createImaginePath(imageName)" :alt="imageName">
+        <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">CHARACTERS</a></li>
+        </ul>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 120px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+header img {
+    max-height: 70%;
+}
+
+header ul {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin: 0;
+}
+
+header a {
+    display: block;
+    padding: 0 0.4rem;
+    color: #303030;
+    line-height: 116px;
+    border-bottom: 4px solid transparent;
+
+}
+
+header a:hover {
+    color: #0282F9;
+    border-bottom: 4px solid #0282F9;
+}
+</style>
