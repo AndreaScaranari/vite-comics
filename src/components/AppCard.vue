7d7fa1bfd2411914bea64884055dcc415dc940e0
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-06 04:29:24 +0100

    comics_in_main_scss_props

---
+++ src/components/AppCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: "AppCard",
+    data: () => ({}),
+    props: {
+        dcComics: Array,
+    },
+    methods: {
+        createImagePath(imgName) {
+            const url = new URL(`../assets/img/${imgName}`, import.meta.url);
+            return url.href
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-for="comic in dcComics" class="col">
+        <div class="card-img">
+            <img :src="comic.thumb" alt="">
+        </div>
+        <div class="card-text">
+            <h3>{{ comic.series }}</h3>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.col {
+    width: calc(100% / 6);
+    padding: 10px;
+
+    .card-img {
+        width: 100%;
+
+        img {
+            width: 100%;
+            aspect-ratio: 1 / 1;
+            object-fit: cover;
+            object-position: 0% 0%;
+        }
+
+    }
+
+    h3 {
+        padding-top: 1rem;
+        font-size: 0.8rem;
+        text-transform: uppercase;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,23 @@
 <script>
 import AppBluestripe from "@/components/AppBluestripe.vue";
+import AppCard from "@/components/AppCard.vue";
+
 
 export default {
     name: "AppMain",
-    components: { AppBluestripe }
+    components: { AppBluestripe, AppCard },
+    props: {
+        dcComics: Array,
+    }
 }
 </script>
 
 <template>
     <main>
         <div class="container">
-            <h1>Content goes here</h1>
+            <div class="row">
+                <AppCard :dcComics="dcComics" />
+            </div>
         </div>
         <AppBluestripe />
     </main>
@@ -20,8 +27,11 @@ export default {
 main {
     color: white;
 
-    .container {
-        height: 120px;
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 2rem 0rem;
+        margin: 0 -10px;
     }
 }
 </style>
